7a2decad2630aafb63dd1548f99c882c547fdfc6
Isa-josep <isaurini1902@gmail.com>
2024-05-01 15:41:45 -0600

    config

---
+++ lib/config/router/app_router.dart
@@ -0,0 +1,13 @@
+import 'package:go_router/go_router.dart';
+import 'package:vetapp/features/presentation/screens.dart';
+
+
+final appRouter =GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const LoginScreen(),
+    ),
+  ]
+);
