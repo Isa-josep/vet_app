467462a45f5d60b093d8a149a90a775ad7791a79
Isa-josep <isaurini1902@gmail.com>
2024-05-04 23:50:25 -0600

    login screen

--- lib/features/presentation/screens/login_screen.dart
+++ lib/features/presentation/screens/login_screen.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
+import 'package:vetapp/features/presentation/widgets.dart';
 
 class LoginScreen extends StatelessWidget {
   const LoginScreen({super.key});
@@ -9,36 +10,44 @@ class LoginScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
-      ),
+      
       body: Center(
         child: Container(
+          
           color: const Color.fromARGB(227, 241, 241, 241),
           height: size.height,
           width: size.width,
           child: Stack(
             alignment: Alignment.bottomRight,
             children: [
+              
+
               SvgPicture.asset(
                 'assets/Vector.svg',
                 fit: BoxFit.cover,
               ),
-               
+               Container(
+                  
+                  margin: EdgeInsets.only(right: size.width*.7, bottom: size.height*.9),
+                 child: SvgPicture.asset(
+                  'assets/Group 1.svg',
+                  fit: BoxFit.cover,
+                               ),
+               ),
                 ClipRRect(
                   child: BackdropFilter(
                   filter: ImageFilter.blur(sigmaX: 1, sigmaY: 1),
                   child: Container(
                   padding: const EdgeInsets.only(left: 20, right: 20, top: 20, bottom: 0),
                   margin: EdgeInsets.only(right: size.width*.05, bottom: size.height*.05),
-                  height: size.height*.4,
+                  height: size.height*0.37,
                   width: size.width*.9,
                   
                   decoration: BoxDecoration(
-                    border: Border.all(color: Colors.black, width: 1.5),
+                    // border: Border.all(color: Colors.black, width: 1.5),
                     borderRadius: BorderRadius.circular(20),
                    
-                    color: const Color(0xFFFFFFFF).withOpacity(.6),
+                    color: const Color.fromARGB(255, 229, 219, 243).withOpacity(.7),
                     // boxShadow:   const [
                     //   BoxShadow(
                     //     color: Colors.black26,
@@ -51,30 +60,35 @@ class LoginScreen extends StatelessWidget {
                     child: Column(
                       children: [
                         const Text("Bienvenido a VetApp", style: TextStyle(color: Colors.black, fontSize: 30),),
-                        TextField(
-                          
-                          decoration: InputDecoration(
-                            hintText: 'Email',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                            ),
-                          ),
+                        const SizedBox(height: 20,),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: CustomFilledButton(
+                            text: 'Ingresar',
+                            buttonColor: const Color.fromARGB(255, 114, 60, 252),
+                            onPressed: (){
+                              //ref.read(loginFormProvider.notifier).onFormSubmit();
+                              // context.push('/home_view'); //TODO: Cambiar por la ruta correcta
+                            },
+                          )
                         ),
-                       const SizedBox(height: 10,),
-                        TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Password',
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(10),
-                              
-                            ),
-                          ),
+                        const SizedBox(height: 25,),
+                        SizedBox(
+                          width: double.infinity,
+                          height: 50,
+                          child: CustomFilledButton(
+                            text: 'Escanear Codigo QR',
+                            textColor: const Color.fromARGB(255, 52, 52, 52),
+                            buttonColor: const Color.fromARGB(255, 244, 239, 239),
+                            onPressed: (){
+                              //ref.read(loginFormProvider.notifier).onFormSubmit();
+                              // context.push('/home_view'); //TODO: Cambiar por la ruta correcta
+                            },
+                          )
                         ),
-                        const SizedBox(height: 10,),
-                        ElevatedButton(
-                          onPressed: () {},
-                          child: const Text('Login'),
-                        ),
+                        const SizedBox(height: 30),
+                        const Text("Necesitas ayuda?" , style: TextStyle(color: Colors.black, fontSize: 17),)
                       ],
                     ),
                   ),
