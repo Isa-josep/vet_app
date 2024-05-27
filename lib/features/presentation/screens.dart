2930badf74e824c36cffb94c2b75e4d229f902d7
Isa-josep <isaurini1902@gmail.com>
2024-05-26 20:41:02 -0600

    barril screens

--- lib/features/presentation/screens.dart
+++ lib/features/presentation/screens.dart
@@ -1,4 +1,10 @@
+
+export 'screens/datapet.dart';
 export 'screens/initial_screen.dart';
 export 'screens/login_Screen.dart';
-// export 'screens/test.dart';
-// export 'screens/test2.dart';
+export 'screens/petpage.dart';
+export 'screens/profileveterinario.dart';
+export 'screens/vacunas_screen.dart';
+export 'screens/veterinario_page.dart';
+
+
